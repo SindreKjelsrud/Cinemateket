f925b8788377e5a0d89b05361376afe4b764fc13
haraldnilsen <harald_998@hotmail.com>
2023-09-15 11:52:44 +0200

    :card_file_box: created dbcontext
    Co-authored-by: Sindre Kjelsrud <kjelsrudsindre@gmail.com>

---
+++ backend/Context/DbContext.cs
@@ -0,0 +1,11 @@
+using backend;
+using Microsoft.EntityFrameworkCore;
+
+public class MovieDbContext: DbContext
+{
+    public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Movie> Movies { get; set; }
+}
