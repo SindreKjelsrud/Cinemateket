0fdc0e6af5902a1f648c248a19595a881b1bdaf0
haraldnilsen <harald_998@hotmail.com>
2023-09-18 15:23:20 +0200

    :card_file_box: automatically add data to db when running backend
    Co-authored-by: Sindre Kjelsrud <kjelsrudsindre@gmail.com>

--- backend/Models/db/MovieDB.cs
+++ backend/Models/db/MovieDB.cs
@@ -1,10 +1,12 @@
 #nullable disable
 
 using Microsoft.EntityFrameworkCore;
+using CsvHelper.Configuration;
+using System.Globalization;
 
 namespace backend;
 
-[PrimaryKey(nameof(Id))]
+// [PrimaryKey(nameof(Id))]
 public class MovieDB
 {
     public int Id { get; set; }
@@ -18,4 +20,14 @@ public class MovieDB
     public string Type { get; set; }
 
     public string Poster { get; set; }
+}
+
+
+public class MovieDBMap : ClassMap<MovieDB>
+{
+    public MovieDBMap()
+    {
+        AutoMap(CultureInfo.InvariantCulture);
+        Map(m => m.Id).Ignore();
+    }
 }
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using CsvHelper;
+using backend;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +19,26 @@ var configuration = builder.Configuration;
 
 builder.Services.AddDbContext<MovieDbContext>(options =>
     options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+var services = builder.Services.BuildServiceProvider();
+using (var scope = services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<MovieDbContext>();
+    context.Database.EnsureCreated();
+
+    // Check if movies are already inserted to avoid duplicate insertion
+    if (!context.Movies.Any())
+    {
+        using (var reader = new StreamReader("public/DbMockData.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            csv.Context.RegisterClassMap<MovieDBMap>();
+            var records = csv.GetRecords<MovieDB>().ToList();
+            context.Movies.AddRange(records);
+            context.SaveChanges();
+        }
+    }
+}
 
 var app = builder.Build();
 
